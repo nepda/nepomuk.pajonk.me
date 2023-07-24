cbd9582059f649fb824a3199ff92deec92e03957
Nepomuk Pajonk <nepomuk@pajonk.me>
2023-07-24 14:04:21 +0200

    Update server-setup.sh

--- scripts/server-setup.sh
+++ scripts/server-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install it with:
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/nepda/nepomuk.me/main/scripts/server-setup.sh)"
+
 sudo apt install zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
